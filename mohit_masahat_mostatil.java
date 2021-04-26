81f00c5c215bcf068ad9922c959bc9189d558a8b
مهدی کارگر <82155434+mahdikargar-cmd@users.noreply.github.com>
2021-04-26 16:15:12 +0430

    Add files via upload

---
+++ mohit_masahat_mostatil.java
@@ -0,0 +1,18 @@
+package practice;
+import java.util.Scanner;
+public class mohit_masahat_mostatil {
+	public static void main(String[] args) {
+ Scanner mahdi=new Scanner(System.in);
+ System.out.println("enter a integer number for tool mostatil:");
+ int tool=mahdi.nextInt();
+ System.out.println("enter a integer number for arz mostatil:");
+ int arz=mahdi.nextInt();
+ int masahat=tool*arz;
+ int mohit=(tool+arz)*2;
+ System.out.println("masahat mostatil: "+masahat);
+ System.out.println("mohite mostatil: "+mohit);
+ 
+ 
+	}	
+	}
+
